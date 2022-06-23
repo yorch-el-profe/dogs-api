4ff51f57cc2efa76b297715af87f9744ec5343ee
Jorge Ramón <jorge.ramon2128@gmail.com>
2022-06-22 21:38:42 -0500

    Agregando try-catch en caso de error del API

--- index.js
+++ index.js
@@ -9,19 +9,27 @@ app.use(cors());
 const API = "https://dog.ceo/api";
 
 app.get("/breeds", async function (request, response) {
-  const { data } = await axios.get(`${API}/breeds/list/all`);
-  const { message } = data;
-  response.json({ data: Object.keys(message) });
+	try {
+		const { data } = await axios.get(`${API}/breeds/list/all`);
+		const { message } = data;
+		response.json({ data: Object.keys(message) });
+	} catch (e) {
+		response.status(400).json({ error: e.message });
+	}
 });
 
 app.get("/breeds/:breed/images", async function (request, response) {
-  const { breed } = request.params;
-  const { n } = request.query;
+	const { breed } = request.params;
+	const { n } = request.query;
 
-  const { data } = n
-    ? await axios.get(`${API}/breed/${breed}/images/random/${n}`)
-    : await axios.get(`${API}/breed/${breed}/images/random`);
-  response.json({ data: data.message });
+	try {
+		const { data } = n
+			? await axios.get(`${API}/breed/${breed}/images/random/${n}`)
+			: await axios.get(`${API}/breed/${breed}/images/random`);
+		response.json({ data: data.message });
+	} catch (e) {
+		response.status(400).json({ error: e.message });
+	}
 });
 
 app.listen(process.env.PORT || 3000);
